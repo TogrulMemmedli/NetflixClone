dc2eb4f1cd3096dd45a6b1610d9f9d28a1a3823e
Togrul Mammadli <togrul.mammed@gmail.com>
2022-07-21 23:42:32 +0400

    Komp versiyonu tamamlandı

---
+++ app.js
@@ -0,0 +1,80 @@
+let i = 0;
+function myFunc() {
+    i++;
+    if (i % 2 == 0) {
+        document.getElementById(String('content01')).style.display = 'none';
+        document.getElementById('plus01').style.transform = 'rotate(0deg)';
+    } else {
+        document.getElementById(String('content01')).style.display = 'block';
+        document.getElementById('plus01').style.transform = 'rotate(45deg)';
+    }
+}
+let j = 0
+function myFunc2() {
+    j++;
+    if (j % 2 == 0) {
+        document.getElementById(String('content02')).style.display = 'none';
+        document.getElementById('plus02').style.transform = 'rotate(0deg)';
+    } else {
+        document.getElementById(String('content02')).style.display = 'block';
+        document.getElementById('plus02').style.transform = 'rotate(45deg)';
+    }
+}
+let k = 0;
+function myFunc3() {
+    k++;
+    if (k % 2 == 0) {
+        document.getElementById(String('content03')).style.display = 'none';
+        document.getElementById('plus03').style.transform = 'rotate(0deg)';
+    } else {
+        document.getElementById(String('content03')).style.display = 'block';
+        document.getElementById('plus03').style.transform = 'rotate(45deg)';
+
+    }
+}
+let q = 0
+function myFunc4() {
+    q++;
+    if (q % 2 == 0) {
+        document.getElementById(String('content04')).style.display = 'none';
+        document.getElementById('plus04').style.transform = 'rotate(0deg)';
+    } else {
+        document.getElementById(String('content04')).style.display = 'block';
+        document.getElementById('plus04').style.transform = 'rotate(45deg)';
+
+    }
+}
+let l = 0;
+function myFunc5() {
+    l++;
+    if (l % 2 == 0) {
+        document.getElementById(String('content05')).style.display = 'none';
+        document.getElementById('plus05').style.transform = 'rotate(0deg)';
+
+    } else {
+        document.getElementById(String('content05')).style.display = 'block';
+        document.getElementById('plus05').style.transform = 'rotate(45deg)';
+    }
+}
+let m = 0
+function myFunc6() {
+    m++;
+    if (m % 2 == 0) {
+        document.getElementById(String('content06')).style.display = 'none';
+        document.getElementById('plus06').style.transform = 'rotate(0deg)';
+    } else {
+        document.getElementById(String('content06')).style.display = 'block';
+        document.getElementById('plus06').style.transform = 'rotate(45deg)';
+    }
+}
+let n = 0;
+function myFunc7() {
+    n++;
+    if (n % 2 == 0) {
+        document.getElementById(String('content07')).style.display = 'none';
+        document.getElementById('plus07').style.transform = 'rotate(0deg)';
+    } else {
+        document.getElementById(String('content07')).style.display = 'block';
+        document.getElementById('plus07').style.transform = 'rotate(45deg)';
+    }
+}
